3e6b651f75192bf0fee490c2484e8b7189cac032
Phr00t <phr00t@gmail.com>
2019-09-05 12:36:11 -0400

    Revert "[Streaming] Load a few initial mipmaps for streamed texture (otherwise they start as completely uninitialized: bad for rendering, and causes crash on Vulkan)"
    This reverts commit 97ca5671dfaca17fd90417587e99163def6383bb.

--- sources/engine/Xenko.Graphics/Data/TextureSerializationData.cs
+++ sources/engine/Xenko.Graphics/Data/TextureSerializationData.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 #pragma warning disable SA1405 // Debug.Assert must provide message text
-using System;
 using System.Diagnostics;
 using Xenko.Core.Annotations;
 using Xenko.Core.Serialization;
@@ -14,13 +13,7 @@ namespace Xenko.Graphics.Data
     /// </summary>
     public sealed class TextureSerializationData
     {
-        internal const int Version = 5;
-
-        /// <summary>
-        /// Texture with a mip map count equal or less than this won't use streaming.
-        /// Also, those levels will be load during initial load, only lower levels will be streamed.
-        /// </summary>
-        internal const int InitialNonStreamedMipCount = 6;
+        internal const int Version = 4;
         
         /// <summary>
         /// The texture image.
@@ -66,10 +59,8 @@ namespace Xenko.Graphics.Data
         /// <param name="stream">The destination stream.</param>
         public void Write(SerializationStream stream)
         {
-            var enableStreaming = EnableStreaming && Image.Description.MipLevels > InitialNonStreamedMipCount;
-
-            stream.Write(enableStreaming);
-            if (enableStreaming)
+            stream.Write(EnableStreaming);
+            if (EnableStreaming)
             {
                 // Write image header
                 ImageHelper.ImageDescriptionSerializer.Serialize(ref Image.Description, ArchiveMode.Serialize, stream);
@@ -77,33 +68,6 @@ namespace Xenko.Graphics.Data
                 // Write storage header
                 Debug.Assert(!string.IsNullOrEmpty(StorageHeader.DataUrl));
                 StorageHeader.Write(stream);
-
-                // Note: in this scenario, we serialize only SkipStreamingMipCount (we know number is strictly higher than this due to previous check)
-                var newDesc = Image.Description;
-                newDesc.MipLevels = InitialNonStreamedMipCount;
-                var pixelBuffers = new PixelBuffer[Image.Description.ArraySize * InitialNonStreamedMipCount];
-
-                // Count number of mip maps that won't be part of initial load (they will be available through streaming)
-                int skippedMipCount = Image.Description.MipLevels - InitialNonStreamedMipCount;
-                for (uint item = 0; item < Image.Description.ArraySize; ++item)
-                {
-                    for (uint level = 0; level < InitialNonStreamedMipCount; ++level)
-                    {
-                        pixelBuffers[item * InitialNonStreamedMipCount + level] = Image.PixelBuffers[item * Image.Description.MipLevels + level + skippedMipCount];
-                    }
-                }
-
-                // Adjust new Width/Height
-                newDesc.Width = pixelBuffers[0].Width;
-                newDesc.Height = pixelBuffers[0].Height;
-
-                var initialImage = new Image
-                {
-                    Description = newDesc,
-                    PixelBuffers = pixelBuffers,
-                };
-                // TODO: We end up duplicating some of the texture data; we could find a way to avoid that by saving only the chunks of higher level mips?
-                initialImage.Save(stream.NativeStream, ImageFileType.Xenko);
             }
             else
             {
