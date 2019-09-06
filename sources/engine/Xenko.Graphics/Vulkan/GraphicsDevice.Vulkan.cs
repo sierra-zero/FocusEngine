20bfa9472303838f3e9c7e8a48e8ff07380c8558
Virgile Bello <virgile@xenko.com>
2019-09-06 11:08:02 +0200

    [Vulkan] Properly store info if a texel buffer need to be integer or float.

--- sources/engine/Xenko.Graphics/Vulkan/GraphicsDevice.Vulkan.cs
+++ sources/engine/Xenko.Graphics/Vulkan/GraphicsDevice.Vulkan.cs
@@ -64,7 +64,7 @@ namespace Xenko.Graphics
             0 // InputAttachment
         };
 
-        internal Buffer EmptyTexelBuffer;
+        internal Buffer EmptyTexelBufferInt, EmptyTexelBufferFloat;
         internal Texture EmptyTexture;
 
         internal PhysicalDevice NativePhysicalDevice => Adapter.GetPhysicalDevice(IsDebugMode);
@@ -366,7 +366,8 @@ namespace Xenko.Graphics
             nativeResourceCollector = new NativeResourceCollector(this);
             graphicsResourceLinkCollector = new GraphicsResourceLinkCollector(this);
 
-            EmptyTexelBuffer = Buffer.Typed.New(this, 1, PixelFormat.R32G32B32A32_Float);
+            EmptyTexelBufferInt = Buffer.Typed.New(this, 1, PixelFormat.R32G32B32A32_UInt);
+            EmptyTexelBufferFloat = Buffer.Typed.New(this, 1, PixelFormat.R32G32B32A32_Float);
             EmptyTexture = Texture.New2D(this, 1, 1, PixelFormat.R8G8B8A8_UNorm_SRgb, TextureFlags.ShaderResource);
         }
 
@@ -456,8 +457,10 @@ namespace Xenko.Graphics
 
         private unsafe void ReleaseDevice()
         {
-            EmptyTexelBuffer.Dispose();
-            EmptyTexelBuffer = null;
+            EmptyTexelBufferInt.Dispose();
+            EmptyTexelBufferInt = null;
+            EmptyTexelBufferFloat.Dispose();
+            EmptyTexelBufferFloat = null;
 
             EmptyTexture.Dispose();
             EmptyTexture = null;
