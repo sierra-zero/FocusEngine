50a97d44e1dd73303b04ac8b0ddeee877ad792d1
Phr00t <phr00t@gmail.com>
2019-11-01 12:47:47 -0400

    global error logging to file, enable via ErrorFileLogger.EnableGlobalExceptionLogger

--- sources/core/Xenko.Core/Diagnostics/LogMessage.cs
+++ sources/core/Xenko.Core/Diagnostics/LogMessage.cs
@@ -44,18 +44,9 @@ namespace Xenko.Core.Diagnostics
         public LogMessage(string module, LogMessageType type, string text, Exception exception, CallerInfo callerInfo)
         {
             if (exception != null) {
-                try {
-                    // Get stack trace for the exception with source file information
-                    StackTrace st = new StackTrace(exception, true);
-                    // Get the top stack frame
-                    StackFrame frame = st.GetFrame(0);
-                    // Get the line number from the stack frame
-                    int line = frame.GetFileLineNumber();
-                    if (line != 0) {
-                        // try to add it to the front of the text
-                        text = "{line #" + line.ToString() + "} " + text;
-                    }
-                } catch (Exception e) { /* couldn't get line info */ }
+                // if file logging is enabled, write it here
+                ErrorFileLogger.WriteExceptionToFile(exception, type.ToString() + ", " + text + ", " + module + ", File: " +
+                                                     (callerInfo?.FilePath ?? "none") + ", Line: " + (callerInfo?.LineNumber.ToString() ?? "none"));
             }
 
             Module = module;
---
+++ sources/core/Xenko.Core/ErrorFileLogger.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Threading;
+
+namespace Xenko.Core
+{
+    /// <summary>
+    /// Enables creating crash logs to file, so it is easier to report crashes
+    /// </summary>
+    public class ErrorFileLogger
+    {
+        private static string savePath = null, savePrefix = null;
+
+        /// <summary>
+        /// Turn on file reporting of all exceptions
+        /// </summary>
+        /// <param name="prefix">What to prefix the files?</param>
+        /// <param name="path">Location to store the log files, null defaults to My Documents folder</param>
+        public static void EnableGlobalExceptionLogger(string prefix = "FocusEngineCrashLog", string path = null)
+        {
+            savePath = (path ?? Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments)) + "/";
+            savePrefix = prefix ?? "FocusEngineCrashLog";
+
+            // Add the event handler for handling non-UI thread exceptions to the event. 
+            AppDomain.CurrentDomain.UnhandledException += new UnhandledExceptionEventHandler(UnhandledException);
+        }
+
+        public static void WriteLogToFile(string message)
+        {
+            if (savePrefix == null || savePath == null) return;
+
+            string time = DateTime.Now.ToString("dd-MMM-yyyy-hh.mm-tt");
+            string filename = savePath + savePrefix + time + ".txt";
+            message = "\n-------------- NEW ENTRY ------------------------\n" +
+                      "Executable: " + System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName + "\n" +
+                      "Time: " + time + "\n" + message;
+            System.IO.File.AppendAllText(filename, message);
+        }
+
+        public static void WriteExceptionToFile(Exception ex, string additionalContext = "")
+        {
+            if (savePrefix == null || savePath == null || ex == null) return;
+
+            try
+            {
+                // Get stack trace for the exception with source file information
+                StackTrace st = new StackTrace(ex, true);
+                // Get the top stack frame
+                StackFrame frame = st.GetFrame(0);
+                // Get the line number from the stack frame
+                int line = frame.GetFileLineNumber();
+                if (line != 0)
+                {
+                    // try to add it to the front of the text
+                    additionalContext += "{line #" + line.ToString() + "}";
+                }
+            }
+            catch (Exception e) { /* couldn't get line info */ }
+
+            string error = "Message: " + ex.Message + "\n" +
+                           "Context: " + additionalContext + "\n" +
+                           "Stack Trace: " + ex.StackTrace + "\n" +
+                           "Source: " + ex.Source + "\n" +
+                           "Inner Exception: " + (ex.InnerException?.Message ?? "None") + "\n" +
+                           "Inner Exception Trace: " + (ex.InnerException?.StackTrace ?? "None");
+            WriteLogToFile(error);
+        }
+
+        private static void UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            WriteExceptionToFile(e.ExceptionObject as Exception);
+        }
+    }
+}
