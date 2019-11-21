78a9e08f1a99d878eb7214c6defa95feb4a65bf4
Phr00t <phr00t@gmail.com>
2019-11-20 22:02:13 -0500

    ThreadPool: performance improvements by reducing overhead and maintaining threads

--- sources/core/Xenko.Core/Threading/ThreadPool.cs
+++ sources/core/Xenko.Core/Threading/ThreadPool.cs
@@ -18,53 +18,36 @@ namespace Xenko.Core.Threading
     internal class ThreadPool
     {
         public static readonly ThreadPool Instance = new ThreadPool();
-        private static TimeSpan maxIdleTime = TimeSpan.FromTicks(5 * TimeSpan.TicksPerSecond);
 
-        private readonly int maxThreadCount = Environment.ProcessorCount + 2;
         private readonly Queue<Action> workItems = new Queue<Action>();
-        private readonly ManualResetEvent workAvailable = new ManualResetEvent(false);
-
-        private readonly Action<object> cachedTaskLoop;
+        private readonly AutoResetEvent workAvailable = new AutoResetEvent(false);
 
         private SpinLock spinLock = new SpinLock();
-        private int workingCount;
-        /// <summary> Usage only within <see cref="spinLock"/> </summary>
-        private int aliveCount;
 
         public ThreadPool()
         {
             // Cache delegate to avoid pointless allocation
-            cachedTaskLoop = (o) => ProcessWorkItems();
+            Action<object> cachedTaskLoop = (o) => ProcessWorkItems();
+            // fire up worker threads
+            for (int i = 0; i < Environment.ProcessorCount; i++)
+                new Task(cachedTaskLoop, null, TaskCreationOptions.LongRunning).Start();
         }
 
         public void QueueWorkItem([NotNull] [Pooled] Action workItem)
         {
             PooledDelegateHelper.AddReference(workItem);
             bool lockTaken = false;
-            bool startNewTask = false;
             try
             {
                 spinLock.Enter(ref lockTaken);
 
                 workItems.Enqueue(workItem);
                 workAvailable.Set();
-
-                var curWorkingCount = Interlocked.CompareExchange(ref workingCount, 0, 0);
-                if (curWorkingCount + 1 >= aliveCount && aliveCount < maxThreadCount)
-                {
-                    startNewTask = true;
-                    aliveCount++;
-                }
             }
             finally
             {
                 if (lockTaken)
                     spinLock.Exit(true);
-            }
-            // No point in wasting spins on the lock while creating the task
-            if (startNewTask)
-            {
-                new Task(cachedTaskLoop, null, TaskCreationOptions.LongRunning).Start();
             }
         }
 
@@ -76,16 +59,14 @@ namespace Xenko.Core.Threading
             {
                 Action workItem = null;
                 var lockTaken = false;
+                int workItemCount;
                 try
                 {
                     spinLock.Enter(ref lockTaken);
-                    int workItemCount = workItems.Count;
+                    workItemCount = workItems.Count;
 
                     if (workItemCount > 0)
-                    {
                         workItem = workItems.Dequeue();
-                        if (workItemCount == 1) workAvailable.Reset(); // we must have taken off our last item
-                    }
                 }
                 finally
                 {
@@ -93,9 +74,9 @@ namespace Xenko.Core.Threading
                         spinLock.Exit(true);
                 }
 
+                // do we have a job to do?
                 if (workItem != null)
                 {
-                    Interlocked.Increment(ref workingCount);
                     try
                     {
                         workItem.Invoke();
@@ -104,16 +85,12 @@ namespace Xenko.Core.Threading
                     {
                         // Ignoring Exception
                     }
-                    Interlocked.Decrement(ref workingCount);
                     PooledDelegateHelper.Release(workItem);
                 }
 
-                // Wait for another work item to be (potentially) available
-                if (workAvailable.WaitOne(maxIdleTime) == false)
-                {
-                    aliveCount--;
-                    return;
-                }
+                // only reset and wait if we took our last job
+                if (workItemCount <= 1)
+                    workAvailable.WaitOne();
             }
         }
     }
