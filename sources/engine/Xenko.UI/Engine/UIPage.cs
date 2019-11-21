71f54407a5ef31859e10b84b3cc0e78c68979625
Phr00t <phr00t@gmail.com>
2019-11-21 16:05:29 -0500

    UI: shortcuts to gathering UIElements from UIPage

--- sources/engine/Xenko.UI/Engine/UIPage.cs
+++ sources/engine/Xenko.UI/Engine/UIPage.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Collections.Generic;
 using Xenko.Core;
 using Xenko.Core.Serialization;
 using Xenko.Core.Serialization.Contents;
@@ -22,5 +23,27 @@ namespace Xenko.Engine
         /// <userdoc>The root element of the page.</userdoc>
         [DataMember]
         public UIElement RootElement { get; set; }
+
+        /// <summary>
+        /// Shortcut to gathering a dictionary of UIElements (keyed by name) from page of type T
+        /// </summary>
+        /// <typeparam name="T">Type of UIElements to gather, can be UIElement too for all</typeparam>
+        /// <param name="dictionary">Dictionary to populate</param>
+        /// <param name="childrenOnly">Include the root, or just the children?</param>
+        public void GatherUIDictionary<T>(ref Dictionary<string, T> dictionary, bool childrenOnly = false) where T : UIElement
+        {
+            RootElement.GatherUIDictionary<T>(ref dictionary, childrenOnly);
+        }
+
+        /// <summary>
+        /// Shortcut to gathering a dictionary of UIElements from page of type T
+        /// </summary>
+        /// <typeparam name="T">Type of UIElements to gather, can be UIElement too for all</typeparam>
+        /// <param name="childrenOnly">Include the root, or just the children?</param>
+        /// <returns>Populated dictionary, keyed by name of UIElement</returns>
+        public Dictionary<string, T> GatherUIDictionary<T>(bool childrenOnly = false) where T : UIElement
+        {
+            return RootElement.GatherUIDictionary<T>(childrenOnly);
+        }
     }
 }
