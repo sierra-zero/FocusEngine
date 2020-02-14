8f8bb48fa48179d75268a09ce755f1fbb4715ca7
Phr00t <phr00t@gmail.com>
2020-02-14 10:54:33 -0500

    Revert "Add additional comment on datagridex"
    This reverts commit 5fe902ad3187b1d24f4f6fe50817eaf231f2cd5b.

--- sources/editor/Xenko.Core.Assets.Editor/View/DataGridEx.cs
+++ sources/editor/Xenko.Core.Assets.Editor/View/DataGridEx.cs
@@ -16,14 +16,6 @@ using Xceed.Wpf.DataGrid;
 namespace Xenko.Core.Assets.Editor.View
 {
     /// <summary>
-    /// Xenko generic wpf data grid. Left empty for future development on a generic datagrid.
-    /// </summary>
-    public class DataGridEx : DataGrid
-    {
-
-    }
-
-    /// <summary>
     /// This class wraps the <see cref="TextSearch"/> class, making accessible all members that are required to make the feature work.
     /// <see cref="TextSearch"/> is massively internal, making this feature impossible to implement by default on custom controls.
     /// </summary>
