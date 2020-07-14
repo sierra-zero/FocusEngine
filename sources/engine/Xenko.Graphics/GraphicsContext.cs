4ca613644d313e1bce6ee45ff2ecffa01922aab8
Phr00t <phr00t@gmail.com>
2020-07-13 21:24:54 -0400

    Graphics: preallocate ResourceGroupAllocators to avoid doing it during runtime

--- sources/engine/Xenko.Graphics/GraphicsContext.cs
+++ sources/engine/Xenko.Graphics/GraphicsContext.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Xenko contributors (https://xenko.com) and Silicon Studio Corp. (https://www.siliconstudio.co.jp)
 // Distributed under the MIT license. See the LICENSE.md file in the project root for more information.
 
+using System.Collections;
+using System.Collections.Generic;
 using Xenko.Core;
 
 namespace Xenko.Graphics
@@ -22,11 +24,26 @@ namespace Xenko.Graphics
 
         public GraphicsResourceAllocator Allocator { get; private set; }
 
+        public static int PrepareAllocatorCount = 32;
+        private static Queue<ResourceGroupAllocator> AvailableAllocators;
+
         public GraphicsContext(GraphicsDevice graphicsDevice, GraphicsResourceAllocator allocator = null, CommandList commandList = null)
         {
             CommandList = commandList ?? graphicsDevice.InternalMainCommandList ?? CommandList.New(graphicsDevice).DisposeBy(graphicsDevice);
             Allocator = allocator ?? new GraphicsResourceAllocator(graphicsDevice).DisposeBy(graphicsDevice);
-            ResourceGroupAllocator = new ResourceGroupAllocator(Allocator, CommandList, 2).DisposeBy(graphicsDevice);
+
+            // prepare some resources now, so we don't need to do it during runtime (which can cause lag spikes or worse)
+            if (AvailableAllocators == null && PrepareAllocatorCount > 0)
+            {
+                AvailableAllocators = new Queue<ResourceGroupAllocator>();
+                while (AvailableAllocators.Count < PrepareAllocatorCount)
+                    AvailableAllocators.Enqueue(new ResourceGroupAllocator(Allocator, CommandList, 2).DisposeBy(graphicsDevice));
+            }
+
+            if (AvailableAllocators?.Count > 0)
+                ResourceGroupAllocator = AvailableAllocators.Dequeue();
+            else
+                ResourceGroupAllocator = new ResourceGroupAllocator(Allocator, CommandList, 2).DisposeBy(graphicsDevice);
         }
     }
 }
