73c500aba8d6949c52a9da08b238fcf092915a90
Virgile Bello <virgile@xenko.com>
2020-07-14 14:47:39 +0200

    [ConnectionRouter] Removed WindowsPhone port mapping code
    # Conflicts:
    #	sources/tools/Xenko.ConnectionRouter/Program.cs
    #	sources/tools/Xenko.ConnectionRouter/WindowsPhoneTracker.cs

--- sources/tools/Xenko.ConnectionRouter/Program.cs
+++ sources/tools/Xenko.ConnectionRouter/Program.cs
@@ -22,7 +22,6 @@ namespace Xenko.ConnectionRouter
         {
             var exeName = Path.GetFileName(Assembly.GetExecutingAssembly().Location);
             var showHelp = false;
-            var windowsPhonePortMapping = false;
             int exitCode = 0;
             string logFileName = "routerlog.txt";
 
@@ -42,7 +41,6 @@ namespace Xenko.ConnectionRouter
                     string.Empty,
                     { "h|help", "Show this message and exit", v => showHelp = v != null },
                     { "log-file=", "Log build in a custom file (default: routerlog.txt).", v => logFileName = v },
-                    { "register-windowsphone-portmapping", "Register Windows Phone IpOverUsb port mapping", v => windowsPhonePortMapping = true },
                 };
 
             try
@@ -57,14 +55,6 @@ namespace Xenko.ConnectionRouter
                 // Make sure path exists
                 if (commandArgs.Count > 0)
                     throw new OptionException("This command expect no additional arguments", "");
-
-                if (windowsPhonePortMapping)
-                {
-                    WindowsPhoneTracker.RegisterWindowsPhonePortMapping();
-                    return 0;
-                }
-
-                SetupTrayIcon(logFileName);
 
                 // Enable file logging
                 if (!string.IsNullOrEmpty(logFileName))
@@ -89,9 +79,6 @@ namespace Xenko.ConnectionRouter
 
                     // Start Android management thread
                     new Thread(() => AndroidTracker.TrackDevices(router)) { IsBackground = true }.Start();
-
-                    // Start Windows Phone management thread
-                    new Thread(() => WindowsPhoneTracker.TrackDevices(router)) { IsBackground = true }.Start();
 
                     //Start iOS device discovery and proxy launcher
                     //Currently this is used only internally for QA testing... as we cannot attach the debugger from windows for normal usages..
