0a86f96f4fb5795e7be8f17a1964c0b332198b09
Phr00t <phr00t@gmail.com>
2020-09-24 10:12:19 -0400

    Revert "Revert "[Rendering] Disabled alpha-to-coverage for Blend and Additive transparency - we want to use the alpha for blending, not testing""
    This reverts commit 46a1b08064d7faea3175ca7a5a289ed45a3acfab.

--- sources/engine/Xenko.Rendering/Rendering/MeshPipelineProcessor.cs
+++ sources/engine/Xenko.Rendering/Rendering/MeshPipelineProcessor.cs
@@ -23,7 +23,7 @@ namespace Xenko.Rendering
                 pipelineState.BlendState = renderMesh.MaterialPass.BlendState ?? BlendStates.AlphaBlend;
                 pipelineState.DepthStencilState = renderMesh.TransparentWriteDepth ? DepthStencilStates.Default : DepthStencilStates.DepthRead;
                 if (isMultisample)
-                    pipelineState.BlendState.AlphaToCoverageEnable = true;
+                    pipelineState.BlendState.AlphaToCoverageEnable = renderMesh.MaterialPass.AlphaToCoverage ?? true;
             }
 
             var cullMode = pipelineState.RasterizerState.CullMode;
