cd1a266607875f4b616428dc7b75ea5e47df4be0
Basewq <Basewq@users.noreply.github.com>
2020-12-11 19:52:32 +1300

    [Input] Fix detecting WinForms right shift key (fixes #754, fixes #929)

--- sources/engine/Xenko.Input/Windows/InputSourceWinforms.cs
+++ sources/engine/Xenko.Input/Windows/InputSourceWinforms.cs
@@ -183,7 +183,7 @@ namespace Xenko.Input
             {
                 // We need to check the scan code to check which SHIFT key it is.
                 var scanCode = (lParam & 0x00FF0000) >> 16;
-                return (scanCode != 36) ? WinFormsKeys.LShiftKey : WinFormsKeys.RShiftKey;
+                return (scanCode != 0x36) ? WinFormsKeys.LShiftKey : WinFormsKeys.RShiftKey;
             }
 
             if (virtualKey == WinFormsKeys.Menu)
