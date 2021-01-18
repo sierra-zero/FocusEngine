fce318f4d143582e6f5891a9c3c05b7af166a5a2
Phr00t <phr00t@gmail.com>
2021-01-17 19:33:14 -0500

    Bump to v3.14.7

--- sources/shared/SharedAssemblyInfo.cs
+++ sources/shared/SharedAssemblyInfo.cs
@@ -30,7 +30,7 @@ namespace Xenko
         /// <summary>
         /// This version will be shown in the editor and actually is the version. Can be changed without triggering weird NuGet behavior.
         /// </summary>
-        public const string VersionToShowInEditor = "3.14.6";
+        public const string VersionToShowInEditor = "3.14.7";
 
         /// <summary>
         /// The current assembly version as text, currently same as <see cref="PublicVersion"/>.
